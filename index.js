f6dc0635cc042f26a518fe354fbddadac5fbaa0b
JeromeADG <113186067+JeromeADG@users.noreply.github.com>
2022-11-25 00:07:54 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+
+var rndm1 = Math.floor(Math.random() * 6) + 1;
+
+var rndmDiceImage = "dice" + rndm1 + ".png";
+
+var rndmImgSrc = "images/" + rndmDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", rndmImgSrc);
+
+
+var rndm2 = Math.floor(Math.random() * 6) + 1;
+
+var rndmImgSrc2 = "images/dice" + rndm2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", rndmImgSrc2);
+
+
+if (rndm1 > rndm2) {
+  document.querySelector("h1").innerHTML = "🚩Player 1 Wins";
+}
+else if (rndm2 > rndm1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins🚩";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
